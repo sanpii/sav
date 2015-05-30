ac453ce109f3d42044ef04812cc2faf76ef81b1a
Sanpi <sanpi@homecomputing.fr>
2015-05-30 10:46:44 +0200

    List untrashed expenses

--- src/application.php
+++ src/application.php
@@ -1,11 +1,27 @@
 <?php
 
+use \PommProject\Foundation\Where;
 use \Symfony\Component\HttpFoundation\Request;
 
 $app = require __DIR__ . '/bootstrap.php';
 
-$app->get('/', function(Request $request) use($app) {
-    return $app['twig']->render('index.html.twig');
+$app->get('/', function (Request $request) use($app) {
+    $page = $request->get('page', 1);
+    $trashed = $request->get('trashed', false);
+    $limit = $request->get('limit', 20);
+
+    $pager = $app['db']->getModel('\Model\ExpenseModel')
+        ->paginateFindWhere(
+            new Where('trashed = $*', [(int)$trashed]),
+            $limit,
+            $page,
+            'ORDER BY created DESC'
+        );
+
+    return $app['twig']->render(
+        'expense/list.html.twig',
+        compact('pager')
+    );
 });
 
 return $app;
