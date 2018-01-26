b32899229b7e74fc2bd631363cf81897aa04476e
Sanpi <sanpi@homecomputing.fr>
2018-01-26 09:18:07 +0100

    Use symfony4

--- config/bundles.php
+++ config/bundles.php
@@ -3,7 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     PommProject\PommBundle\PommBundle::class => ['all' => true],
-    Symfony\Bundle\AsseticBundle\AsseticBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
