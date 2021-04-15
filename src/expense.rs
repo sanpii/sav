52c3a405b5b1b98a3ae8ac6ea0c0d4043b9cd3a0
Sanpi <sanpi@homecomputing.fr>
2021-04-15 16:31:19 +0200

    Updates elephantry to 2.1

--- src/expense.rs
+++ src/expense.rs
@@ -1,5 +1,7 @@
 #[derive(Debug, elephantry::Entity, serde_derive::Serialize)]
+#[elephantry(model = "Model", structure = "Structure", relation = "public.expense")]
 pub struct Entity {
+    #[elephantry(pk)]
     pub id: Option<i32>,
     pub created_at: chrono::NaiveDateTime,
     pub serial: Option<String>,
@@ -8,58 +10,10 @@ pub struct Entity {
     pub shop: String,
     pub warranty: i32,
     pub price: f32,
+    #[elephantry(virtual = "%:created_at:% + (%:warranty:% || ' years')::interval")]
     pub warranty_at: chrono::NaiveDateTime,
+    #[elephantry(virtual = "%:created_at:% + (%:warranty:% || ' years')::interval > now()")]
     pub warranty_active: bool,
+    #[elephantry(virtual = "%:trashed_at:% is not null")]
     pub trashed: bool,
-}
-
-pub struct Model;
-
-impl<'a> elephantry::Model<'a> for Model {
-    type Entity = Entity;
-    type Structure = Structure;
-
-    fn new(_: &'a elephantry::Connection) -> Self {
-        Self {}
-    }
-
-    fn create_projection() -> elephantry::Projection {
-        Self::default_projection()
-            .add_field(
-                "warranty_at",
-                "%:created_at:% + (%:warranty:% || ' years')::interval",
-            )
-            .add_field(
-                "warranty_active",
-                "%:created_at:% + (%:warranty:% || ' years')::interval > now()",
-            )
-            .add_field("trashed", "%:trashed_at:% is not null")
-            .unset_field("trashed_at")
-    }
-}
-
-pub struct Structure;
-
-impl elephantry::Structure for Structure {
-    fn relation() -> &'static str {
-        "public.expense"
-    }
-
-    fn primary_key() -> &'static [&'static str] {
-        &["id"]
-    }
-
-    fn columns() -> &'static [&'static str] {
-        &[
-            "id",
-            "created_at",
-            "serial",
-            "name",
-            "url",
-            "shop",
-            "warranty",
-            "price",
-            "trashed_at",
-        ]
-    }
 }
